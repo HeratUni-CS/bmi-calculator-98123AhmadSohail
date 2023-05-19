81d45841250d52538557ae2085986b42d095cf97
Sohail <ahmadsohailmaghsoudi@gmail.com>
2023-05-19 15:51:30 +0430

    created five containers in the body then instead of repeating the same code we created reusable card and just called it wheneever we needed it

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,10 +10,52 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33)),
+              ),
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33)),
+              ),
+            ],
+          )),
+          Expanded(
+            child: reusableCard(colour: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33)),
+              ),
+              Expanded(
+                child: reusableCard(colour: Color(0xFF1D1E33)),
+              ),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class reusableCard extends StatelessWidget {
+  reusableCard({required this.colour});
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
